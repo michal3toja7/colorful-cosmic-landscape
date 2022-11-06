e2cb169540dd76240fa8d7f72ac7c62d62c1f85d
Michał Bruzdowski <michal3toja7@gmail.com>
2022-11-06 02:17:38 +0100

    make basic generate of stars and stars around basic stars

--- src/services/starsGenerator.ts
+++ src/services/starsGenerator.ts
@@ -1,18 +1,32 @@
 import Star from "../models/star";
 
 const starsGenerator = (width: number, height: number): Star[] => {
+    let listOfStar: Star[] = basicStarsGenerate(width, height, 20, 6)
+    let newStars: Star[] = []
+    for (const star of listOfStar) {
+        const minWidth = (star.posX - 400) ? (star.posY - 400) : 0
+        const maxWidth = (star.posX + 400) <= width ? (star.posX + 400) : width
+        const minHeight = (star.posY - 400) ? (star.posY - 400) : 0
+        const maxHeight = (star.posY + 400) <= height ? (star.posY + 400) : height
+        newStars.push(star)
+        newStars.push(...basicStarsGenerate(maxWidth, maxHeight, 30, star.bright, minWidth, minHeight ))
+    }
+
+    return newStars
+
+}
+
+const basicStarsGenerate = (width: number, height: number, iterations: number = 10, maxBright: number = 8, minWidth: number = 0, minHeight: number = 0) => {
     let listOfStar: Star[] = []
-    for (const x of Array.from(Array(10).keys())) {
-        console.log("dupa")
+    for (const x of Array.from(Array(iterations).keys())) {
         listOfStar.push(
             {
-                posX: getRandomInt(0, width*2),
-                posY: getRandomInt(0, height*2),
-                bright: getRandomInt(1, 8)
+                posX: getRandomInt(minWidth, width * 2),
+                posY: getRandomInt(minHeight, height * 2),
+                bright: getRandomInt(1, maxBright)
             }
         )
     }
-    console.log(listOfStar)
 
     return listOfStar
 
